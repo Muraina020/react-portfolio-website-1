7c5eab031fcc7c21f884d18814e2619f3e10bc54
Muraina020 <glonal-murainaboluwatife@gmail.com>
2023-06-05 22:31:42 +0100

    Change component files

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,7 +6,7 @@ function experience() {
   return (
     <section id='experience'>
       <h5>What Skills I Have</h5>
-      <h2>Mt Experience</h2>
+      <h2>My Experience</h2>
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
